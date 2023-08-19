dc99f7f02aae1e9b1b119a13a5e57e1b82d4690e
ashutosh9897794237 <113291959+ashutosh9897794237@users.noreply.github.com>
2023-08-19 14:54:35 +0530

    gjh

--- script.js
+++ script.js
@@ -48,13 +48,47 @@ function onDeleteRow(e){
     btn.closest("tr").remove();
 
 }
-function onEditRow(e){
-    if(!e.target.classList.contains("editButn")){
-        return;
-    }
-    const btn = e.target;
-    btn.edit("tr").edit_row();
+//function onEditRow(e){
+  //  if(!e.target.classList.contains("editButn")){
+    //    return;
+   // }
+   // const btn = e.target;
+   // btn.edit("tr").edit_row();
 
+// }
+
+function editData(button) {
+            
+    // Get the parent row of the clicked button
+    let row = button.parentNode.parentNode;
+    
+    // Get the cells within the row
+    let nameCell = row.cells[0];
+    let emailCell = row.cells[1];
+    let mobileCell = row.cells[2];
+    let addressCell = row.cells[3];
+    
+    // Prompt the user to enter updated values
+    let nameInput =
+        prompt("Enter the updated name:",
+            nameCell.innerHTML);
+    let emailInput =
+        prompt("Enter the updated email:",
+            emailCell.innerHTML);
+    let numberInput =
+        prompt("Enter the updated mobile details:",
+            mobileCell.innerHTML
+        );
+    let addressInput =
+        prompt("Enter the updated address:",
+            addressCell.innerHTML
+        );
+    
+    // Update the cell contents with the new values
+    nameCell.innerHTML = nameInput;
+    emailCell.innerHTML = emailInput;
+    mobileCell.innerHTML = numberInput;
+    addressCell.innerHTML = addressInput;
 }
     table.addEventListener('click', onDeleteRow);
-    table.addEventListener('click', onEditRow);
+    table.addEventListener('click', editData);
